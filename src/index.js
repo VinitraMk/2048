a9c3e3d828fec1f18794d88ccc17bdb10f08f9a5
Vinitra Muralikrishnan <vimurali@microsoft.com>
2019-09-24 14:02:18 +0530

    game logic added: basic game play and current score

--- src/index.js
+++ src/index.js
@@ -4,21 +4,48 @@ import './index.css';
 
 
 function Square(props) {
-    if(props.value===2){
-        return(
-            <div className="game2048-board__square color-two">{props.value}</div>
-        )
+
+    switch(props.value) {
+        case 0: 
+            return(<div className="game2048-board__square color-zero"></div>);
+            break;
+        case 2:
+            return(<div className="game2048-board__square color-two"><p>{props.value}</p></div>);
+            break;
+        case 4:
+            return(<div className="game2048-board__square color-four"><p>{props.value}</p></div>);
+            break;
+        case 8:
+            return(<div className="game2048-board__square color-eight"><p>{props.value}</p></div>);
+            break;
+        case 16:
+            return(<div className="game2048-board__square color-sixteen"><p>{props.value}</p></div>);
+            break;
+        case 32:
+            return(<div className="game2048-board__square color-thirtytwo"><p>{props.value}</p></div>);
+            break;
+        case 64:
+            return(<div className="game2048-board__square color-sixtyfour"><p>{props.value}</p></div>);
+            break;
+        case 128:
+            return(<div className="game2048-board__square color-onetwoeight"><p>{props.value}</p></div>);
+            break;
+        case 256:
+            return(<div className="game2048-board__square color-twofivesix"><p>{props.value}</p></div>);
+            break;
+        case 512:
+            return(<div className="game2048-board__square color-fiveonetwo"><p>{props.value}</p></div>);
+            break;
+        case 1024:
+            return(<div className="game2048-board__square color-onezerotwofour"><p>{props.value}</p></div>);
+            break;
+        case 2048:
+            return(<div className="game2048-board__square color-twozerofoureight"><p>{props.value}</p></div>);
+            break;
+        default:
+            break;
     }
-    else if(props.value===4) {
-        return(
-            <div className="game2048-board__square color-four">{props.value}</div>
-        )
-    }
-    else {
-        return(
-            <div className="game2048-board__square"></div>
-        )
-    }
+    
 }
 
 class Board extends React.Component {
@@ -34,70 +61,127 @@ class Board extends React.Component {
 
     moveLeftOrUp(line) {
         var values=Array(4).fill(0);
+        var prev_values=Array(4).fill(0);
         var squares=this.state.squares.slice();
         var count=0;
+        var prev=-1;
+        var score=0;
+        var change=true;
 
-        if(this.state.direction=="left") {
+        //Copy all values
+        if(this.state.direction==="left") {
             for(var i=0;i<4;i++) {
-                values[i]=squares[(line*4)+i].props.value;
+                prev_values[i]=values[i]=squares[(line*4)+i].props.value;
             }
         }
-        else if(this.state.direction="up") {
+        else if(this.state.direction==="up") {
             for(i=0;i<4;i++) {
-                values[i]=squares[(i*4)+line].props.value;
+                prev_values[i]=values[i]=squares[(i*4)+line].props.value;
             }
         }
 
+        //Move or Merge 
         for(i=0;i<4;i++) {
             if(values[i]!==0) {
-                values[count++]=values[i];
+                if(prev!==-1) {
+                    if(values[i]===prev) {
+                        values[count-1]*=2;
+                        //this.props.updateScore(prev*2);
+                        score+=(prev*2);
+                        prev=-1;
+                    }
+                    else {
+                        prev=values[i];
+                        values[count++]=values[i];
+                    }
+
+                }
+                else {
+                    prev=values[i];
+                    values[count++]=values[i];
+                }
             }
         }
         for(i=count;i<4;i++) {
             values[i]=0;
         }
-        return values;
+        //this.props.updateScore(score);
+        if(JSON.stringify(prev_values)===JSON.stringify(values)) {
+            change=false;
+        }
+        return [values,score,change];
     }
 
     moveRightOrDown(line) {
         var values=Array(4).fill(0);
+        var prev_values=Array(4).fill(0);
         var squares=this.state.squares.slice();
         var count=0;
+        var prev=-1;
+        var score=0;
+        var change=true;
         
+        //Copy all squares
         if(this.state.direction==="right") {
             for(var i=0;i<4;i++) {
-                values[i]=squares[(line*4)+i].props.value;
+                prev_values[i]=values[i]=squares[(line*4)+i].props.value;
             }
         }
         else if(this.state.direction==="down") {
             for(i=0;i<4;i++) {
-                values[i]=squares[(i*4)+line].props.value;
+                prev_values[i]=values[i]=squares[(i*4)+line].props.value;
             }
         }
+
+        //Move or Merge
         for(i=3;i>=0;i--) {
             if(values[i]!==0) {
-                values[3-count]=values[i];
-                count++;
+                if(prev!==-1) {
+                    if(prev===values[i]) {
+                        values[3-(count-1)]*=2;
+                        //this.props.updateScore(prev*2);
+                        score+=(prev*2);
+                        prev=-1;
+                    }
+                    else {
+                        prev=values[i];
+                        values[3-count]=values[i];
+                        count++;
+                    }
+                }
+                else {
+                    prev=values[i];
+                    values[3-count]=values[i];
+                    count++;
+                }
             }
         }
         for(i=(3-count);i>=0;i--) {
             values[i]=0;
         }
+
+        if(JSON.stringify(prev_values)===JSON.stringify(values)) {
+            change=false;
+        }
         
-        return values;
+        return [values,score,change];
     }
 
     
     moveSquares() {
         var values=Array(4).fill(0);
         var squares=this.state.squares.slice();
-        console.log(this.state.direction);
+        var score=0;
+        var change=false;
         
 
         //Move All squares to the left
         if(this.state.direction==="left") {
             for(var row=0;row<4;row++) {
-                values=this.moveLeftOrUp(row);
+                var arr=this.moveLeftOrUp(row);
+                values=arr[0];
+                score+=arr[1];
+                change|=arr[2];
                 for(var i=0;i<4;i++) {
                     squares[(row*4)+i]=<Square value={values[i]}/>;
                 }
@@ -107,7 +191,10 @@ class Board extends React.Component {
         //Move all squares to right
         if(this.state.direction==="right") {
             for(row=0;row<4;row++) {
-                values=this.moveRightOrDown(row);
+                arr=this.moveRightOrDown(row);
+                values=arr[0];
+                score+=arr[1];
+                change|=arr[2];
                 for(i=0;i<4;i++) {
                     squares[(row*4)+i]=<Square value={values[i]}/>;
                 }
@@ -117,16 +204,23 @@ class Board extends React.Component {
         //Move all squares up
         if(this.state.direction==="up") {
             for(var col=0;col<4;col++) {
-                values=this.moveLeftOrUp(col);
+                arr=this.moveLeftOrUp(col);
+                values=arr[0];
+                score+=arr[1];
+                change|=arr[2];
                 for(i=0;i<4;i++) {
                     squares[(i*4)+col]=<Square value={values[i]}/>;
                 }
             }
         }
 
+        //Move all squares down
         if(this.state.direction==="down") {
             for(col=0;col<4;col++) {
-                values=this.moveRightOrDown(col);
+                arr=this.moveRightOrDown(col);
+                values=arr[0];
+                score+=arr[1];
+                change|=arr[2];
                 for(i=0;i<4;i++) {
                     squares[(i*4)+col]=<Square value={values[i]}/>;
                 }
@@ -134,6 +228,26 @@ class Board extends React.Component {
         }
 
         //Assign new squares position
+        this.setState({squares:squares},()=>{
+            this.props.updateScore(score);
+            if(change) {
+                this.bringNewSquare();
+            }
+        });
+    }
+
+    bringNewSquare() {
+        var squares=this.state.squares.slice();
+        var empty=[];
+        var init=[2,4];
+        for(var i=0;i<squares.length;i++) {
+            if(squares[i].props.value===0) {
+                empty.push(i);
+            }
+        }
+        var ind=Math.floor(Math.random()*empty.length);
+        var in1=Math.round(Math.random());
+        squares[empty[ind]]=<Square value={init[in1]}/>;
         this.setState({squares:squares});
     }
 
@@ -157,9 +271,10 @@ class Board extends React.Component {
                 break;
         }
         
-        this.setState({direction:direction});
-        this.moveSquares();
+        this.setState({direction:direction},this.moveSquares);
+        
     }
+
 
     initBoard () {
         var indices=[];
@@ -174,13 +289,14 @@ class Board extends React.Component {
         for(var i=0;i<16;i++){
             if(indices[0]===i || indices[1]===i) {
                 var num=Math.round(Math.random());
-                squares[i]=<Square value={init[num]}/>
+                squares[i]=<Square value={init[num]}/>;
             }
             else {
-                squares[i]=<Square value={0}/>
+                squares[i]=<Square value={0}/>;
             }
         }
         this.setState({squares:squares});
+        document.getElementById('main-board').classList.toggle('is-in');
     }
 
     componentDidMount() {
@@ -192,7 +308,7 @@ class Board extends React.Component {
 
     render() {
         return (
-            <div className="game2048-board">
+            <div className="game2048-board" id="main-board">
                 {this.state.squares}
             </div>
         )
@@ -209,7 +325,12 @@ class Game2048 extends React.Component {
         };
     }
 
-
+    updateScore=(value) => {
+        var tv=parseInt(value);
+        var current_score=this.state.current_score;
+        current_score+=tv;
+        this.setState({current_score:current_score});
+    }
 
     
     render() {
@@ -232,7 +353,7 @@ class Game2048 extends React.Component {
                 <div className="game2048-action">
                     <button className="game2048-action__button" type="button">New Game</button>
                 </div>
-                <Board/>
+                <Board updateScore={this.updateScore}/>
             </div>
         )
     }
